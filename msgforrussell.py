770a68b1de4980d35f05a94b108d9b78842db747
russellmyers <russell.myers@ngahr.com>
2019-07-09 19:07:24 +1000

    version4
    Fix bug in previous version

--- msgforrussell.py
+++ msgforrussell.py
@@ -1,9 +1,8 @@
 def what_is_russell(adjective):
-    print("Russell is {}.".format(adjective))
+    print("Russell is definitely not {}.".format(adjective))
 
 
 what_is_russell("gay")
-fghdfgh
 
 
 
